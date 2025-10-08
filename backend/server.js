383b789162bfa3e863048a6b88f793fae3c4358d
Shiva Pareek <pareekshiva2004@gmail.com>
2025-10-08 12:04:07 +0530

    Fix: added path import and corrected build path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/database');
+const path = require('path');
+
 
 // Import routes
 const studentRoutes = require('./routes/students');
